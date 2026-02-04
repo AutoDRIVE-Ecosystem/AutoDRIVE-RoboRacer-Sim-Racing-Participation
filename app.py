77f43421b40ae8252ac6e9ddd719a196f55ea2d1
Tinker Twins <tinkertwinsofficial@gmail.com>
2026-02-04 01:13:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def update_map(selected_comp, selected_metric):
 # Run app
 # -------------------------
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=port, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=port, threaded=True)
